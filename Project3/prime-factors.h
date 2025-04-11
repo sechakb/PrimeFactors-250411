22a0d52bf0523f38083378c97fdde4ef2e192036
kdh <dongri.kang@sansumg.com>
2025-04-11 13:56:58 +0900

    [refactoring] 42p

--- Project3/prime-factors.h
+++ Project3/prime-factors.h
@@ -9,13 +9,9 @@ public:
     vector<int> of(int num)
     {
         vector<int> result = {};
-        if (num == 2)
+        if (num > 1)
         {
-            result.push_back(2);
-        }
-        else if (num == 3)
-        {
-            result.push_back(3);
+            result.push_back(num);
         }
         return result;
     }
