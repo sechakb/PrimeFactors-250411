b57021ccdbc80bdfecaf07d0d415ea501658a169
kdh <dongri.kang@sansumg.com>
2025-04-11 14:04:26 +0900

    [refactoring] 52p

--- Project3/prime-factors.h
+++ Project3/prime-factors.h
@@ -11,18 +11,25 @@ public:
         vector<int> result = {};
         if (num > 1)
         {
+            int divisor = 2;
             if (num == 4)
             {
-                while (num % 2 == 0)
+                while (num % divisor == 0)
                 {
-                    result.push_back(2);
-                    num /= 2;
+                    result.push_back(divisor);
+                    num /= divisor;
                 }
             }
             else if (num == 6)
             {
-                result.push_back(2);
-                result.push_back(3);
+                for (divisor = 2; num > 1; divisor++)
+                {
+                    while (num % divisor == 0)
+                    {
+                        result.push_back(divisor);
+                        num /= divisor;
+                    }
+                }
             }
             else
             {
